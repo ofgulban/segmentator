b1bf6b7668e2a30b70f447dce37c51d4079a7e95
marian <marian@FDPMW2378.UNIMAAS>
2016-05-26 20:38:03 +0200

    Attribution re-added.

--- sector_mask.py
+++ sector_mask.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# This code is taken from user 'ali_m' from StackOverflow:
+# <http://stackoverflow.com/questions/18352973/mask-a-circular-sector-in-a-numpy-array>
 
 import numpy as np
 
