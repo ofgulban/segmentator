484d60b4c93fffd85e0f8ad52c42bb0122af7fb0
ofgulban <farukgulban@gmail.com>
2016-09-26 15:14:32 +0200

    Cosmetics.

--- config.py
+++ config.py
@@ -18,5 +18,5 @@
 
 # define variables used to initialise the sector mask
 init_centre = (0, 0)
-init_radius = 200
+init_radius = 100
 init_theta = (0, 360)
--- deriche_prepare.py
+++ deriche_prepare.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filename',  metavar='path',
                     help="Path to a 3D nifti file.")
 
-parser.add_argument("--alpha", metavar='2',
+parser.add_argument("--alpha", "-a", metavar='2',
                     required=False, default=2, type=float,
                     help="Alpha controls smoothing, lower -> smoother")
 
