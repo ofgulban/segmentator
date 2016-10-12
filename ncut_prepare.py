eea9b5a66863dc4c7c2d1c6cad4f96ce6ba14ed3
ofgulban <farukgulban@gmail.com>
2016-10-12 14:53:40 +0200

    Change default value.

--- ncut_prepare.py
+++ ncut_prepare.py
@@ -30,8 +30,8 @@ parser.add_argument("--nrSupPix", "-sp", metavar='2500',
                     required=False, default=2500, type=int,
                     help="Number of regions/superpixels.")
 
-parser.add_argument("--compactness", "-c", metavar='5',
-                    required=False, default=5, type=float,
+parser.add_argument("--compactness", "-c", metavar='2',
+                    required=False, default=2, type=float,
                     help="Compactness balances intensity proximity and space \
                     proximity of the superpixels. \
                     Higher values give more weight to space proximity, making \
