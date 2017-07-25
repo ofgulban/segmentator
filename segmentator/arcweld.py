db478426dbcc9fd2a483da5035de590c223034cd
ofgulban <farukgulban@gmail.com>
2017-07-25 15:33:41 +0200

    In progress... - Weighting parameters are added to allow further tweaking. - Zero-max arc is definition is changed.

--- segmentator/arcweld.py
+++ segmentator/arcweld.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from nibabel import load, Nifti1Image, save
 
 # load
-nii = load('/home/faruk/gdrive/Segmentator/data/faruk/arcweld/S02_T1wDivPD_bet_nosub_aniso.nii.gz')
+nii = load('/home/faruk/gdrive/Segmentator/data/faruk/mp2rage_roy/S001_UNI_restore_aniso.nii.gz')
 ima = nii.get_data()
 basename = nii.get_filename().split(os.extsep, 1)[0]
 
@@ -23,11 +23,12 @@ ima, gra = ima.flatten(), gra.flatten()
 msk = ima > 0
 
 # select low gradient magnitude regime (lr)
-msk_lr = (gra < np.percentile(gra[msk], 10)) & (msk)
+msk_lr = (gra < np.percentile(gra[msk], 25)) & (msk)
 n, bins, _ = plt.hist(ima[msk_lr], 100, range=(0, ima_max))
 
 # detect 'pure tissue' peaks (TODO: Checks for always finding 3 peaks)
-peaks = peakutils.indexes(n, thres=0.25/max(n), min_dist=20)
+peaks = peakutils.indexes(n, thres=0.25/max(n), min_dist=10)
+# peaks = peaks[0:-1]
 tissues = []
 for p in peaks:
     tissues.append(bins[p])
@@ -37,8 +38,9 @@ tissues = np.array(tissues)
 # tissues = np.append(tissues, tissues[-1] + (tissues[-1] - tissues[-2]) / 2)
 
 # insert zero-max arc
-zmax_center = (0 + ima_max) / 2
-zmax_radius = ima_max - zmax_center
+zmax_max = tissues[-1] + tissues[-1]-tissues[-2]
+zmax_center = (0 + zmax_max) / 2.
+zmax_radius = zmax_max - zmax_center
 tissues = np.append(tissues, zmax_center)
 
 # create smooth maps (distance to pure tissue)
@@ -47,21 +49,31 @@ soft = []  # to hold soft tissue membership maps
 for i, t in enumerate(tissues):
     tissue = np.array([t, 0])
     # euclidean distance
-    edist = np.sqrt(np.sum((voxels - tissue[:, None])**2, axis=0))
+    edist = np.sqrt(np.sum((voxels - tissue[:, None])**2., axis=0))
     soft.append(edist)
     # save intermediate maps
     out = Nifti1Image(edist.reshape(nii.shape), affine=nii.affine)
     save(out, basename + '_t' + str(i) + '.nii.gz')
 soft = np.array(soft)
 
-# interface translation
-soft[-1, :] = np.abs(soft[-1, :] - zmax_radius)
+# interface translation (shift zero circle to another radius)
+soft[-1, :] = soft[-1, :] - zmax_radius
+# zmax_neg = soft[-1, :] < 0  # voxels fall inside zero-max arc
 
+# weight zero-max arc to not coincide with pure class regions
+# zmax_weight = (gra[zmax_neg] / zmax_radius)**-1
 zmax_weight = (gra / zmax_radius)**-1
-# zmax_weight = (((gra / zmax_radius) + (ima / tissues[-1])) / 2)**-1
-soft[-1, :] = zmax_weight*soft[-1, :]
+# soft[-1, :][zmax_neg] = zmax_weight*np.abs(soft[-1, :][zmax_neg])
+soft[-1, :] = zmax_weight*np.abs(soft[-1, :])
 out = Nifti1Image(soft[-1, :].reshape(nii.shape), affine=nii.affine)
 save(out, basename + '_zmaxarc' + '.nii.gz')
+
+# arbitrary weighting (TODO: Can be turned into config file of some sort)
+# save these values for MPRAGE
+# soft[0, :] = soft[0, :] * 0.66  # csf
+# soft[1, :] = soft[1, :] * 1.  # gm
+# soft[2, :] = soft[2, :] * 1.25  # wm
+# soft[3, :] = soft[3, :] * 0.5  # zero-max arc
 
 # hard tissue membership maps
 hard = np.argmin(soft, axis=0)
