539c18e54ec4263acce8bc678275241c3d0bfe0c
ofgulban <farukgulban@gmail.com>
2017-09-22 12:24:58 +0200

    Defaults in ui are collected from config file now

--- segmentator/__main__.py
+++ segmentator/__main__.py
@@ -12,7 +12,7 @@ looks a bit messy as is.
 
 import sys
 import argparse
-import config
+import config as cfg
 
 
 def main(args=None):
@@ -26,8 +26,8 @@ def main(args=None):
         help="Path to input. Mostly a nifti file with image data."
         )
     parser.add_argument(
-        "--gramag", metavar='3D_scharr', required=False,
-        default=config.gramag,
+        "--gramag", metavar=str(cfg.gramag), required=False,
+        default=cfg.gramag,
         help="'3D_scharr', '3D_sobel', '3D_prewitt', 'numpy' \
         or path to a gradient magnitude nifti."
         )
@@ -36,28 +36,28 @@ def main(args=None):
         help="Path to nyp file with ncut labels"
         )
     parser.add_argument(
-        "--scale", metavar='400', required=False, type=float,
-        default=config.scale,
+        "--scale", metavar=str(cfg.scale), required=False, type=float,
+        default=cfg.scale,
         help="Data is scaled from 0 to this number."
         )
     parser.add_argument(
-        "--percmin", metavar='2.5', required=False, type=float,
-        default=config.perc_min,
+        "--percmin", metavar=str(cfg.perc_min), required=False, type=float,
+        default=cfg.perc_min,
         help="Minimum percentile used in truncation."
         )
     parser.add_argument(
-        "--percmax",  metavar='97.5', required=False,  type=float,
-        default=config.perc_max,
+        "--percmax",  metavar=str(cfg.perc_max), required=False,  type=float,
+        default=cfg.perc_max,
         help="Maximum percentile used in truncation."
         )
     parser.add_argument(
-        "--cbar_max",  metavar='5.0', required=False,  type=float,
-        default=config.cbar_max,
+        "--cbar_max",  metavar=str(cfg.cbar_max), required=False,  type=float,
+        default=cfg.cbar_max,
         help="Maximum value (power of 10) of the colorbar slider."
         )
     parser.add_argument(
-        "--cbar_init",  metavar='2.0', required=False,  type=float,
-        default=config.cbar_init,
+        "--cbar_init",  metavar=str(cfg.cbar_init), required=False,
+        type=float, default=cfg.cbar_init,
         help="Initial value (power of 10) of the colorbar slider. \
               Also used with --ncut_prepare flag."
         )
@@ -82,17 +82,17 @@ def main(args=None):
         )
     parser.add_argument(
         "--ncut_maxRec", required=False, type=int,
-        default=config.max_rec, metavar=config.max_rec,
+        default=cfg.max_rec, metavar=cfg.max_rec,
         help="Maximum number of recursions."
         )
     parser.add_argument(
         "--ncut_nrSupPix", required=False, type=int,
-        default=config.nr_sup_pix, metavar=config.nr_sup_pix,
+        default=cfg.nr_sup_pix, metavar=cfg.nr_sup_pix,
         help="Number of regions/superpixels."
         )
     parser.add_argument(
         "--ncut_compactness", required=False, type=float,
-        default=config.compactness, metavar=config.compactness,
+        default=cfg.compactness, metavar=cfg.compactness,
         help="Compactness balances intensity proximity and space \
         proximity of the superpixels. \
         Higher values give more weight to space proximity, making \
@@ -101,26 +101,26 @@ def main(args=None):
         objects in the image."
         )
 
-    # set config file variables to be accessed from other scripts
+    # set cfg file variables to be accessed from other scripts
     args = parser.parse_args()
     # used in all
-    config.filename = args.filename
+    cfg.filename = args.filename
     # used in segmentator GUI (main and ncut)
-    config.gramag = args.gramag
-    config.scale = args.scale
-    config.perc_min = args.percmin
-    config.perc_max = args.percmax
-    config.cbar_max = args.cbar_max
-    config.cbar_init = args.cbar_init
+    cfg.gramag = args.gramag
+    cfg.scale = args.scale
+    cfg.perc_min = args.percmin
+    cfg.perc_max = args.percmax
+    cfg.cbar_max = args.cbar_max
+    cfg.cbar_init = args.cbar_init
     if args.include_zeros:
-        config.discard_zeros = False
+        cfg.discard_zeros = False
     # used in ncut preparation
-    config.ncut_figs = args.ncut_figs
-    config.max_rec = args.ncut_maxRec
-    config.nr_sup_pix = args.ncut_nrSupPix
-    config.compactness = args.ncut_compactness
+    cfg.ncut_figs = args.ncut_figs
+    cfg.max_rec = args.ncut_maxRec
+    cfg.nr_sup_pix = args.ncut_nrSupPix
+    cfg.compactness = args.ncut_compactness
     # used in ncut
-    config.ncut = args.ncut
+    cfg.ncut = args.ncut
 
     print("===========\nSegmentator\n===========")
 
